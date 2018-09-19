f02aa2a9f03ed4e7cbe37a2bad497751f346abc3
John Glatts <33476958+jglatts@users.noreply.github.com>
2018-09-18 21:50:29 -0700

    Update cis_hw_two.c

--- cis_hw_two.c
+++ cis_hw_two.c
@@ -3,7 +3,7 @@
 CIS 1057 HW 2
 Author: John Glatts
 Program: Triangle Determanation and Area Calculation
-Contact: tug13361@temple.edu
+Contact: tug13361@gmail.com
 Due Date: 
 
 */
@@ -15,7 +15,6 @@ Due Date:
 
 int main(int argc, char const *argv[]) {
 	// Ask user for input and store them in double variables 
-	// 
 	double side_one, side_two, side_three;  //user input for triangle sides	
 
 	printf("\nEnter Length of Side 1\n");
@@ -51,30 +50,31 @@ int triangleValid(s_one, s_two, s_three){
 
 	if(s_one + s_two > s_three && s_one + s_three > s_two && s_two + s_three > s_one)
 	{
-		//Get longest side
 		printf("\n\n\nVALID Triangle");
 		//Get longest side
 		if(s_one > s_two && s_one > s_three){
-			longest_side = s_one;
+			longest_side = s_one;	
+			s_one = s_three; //this allows user to enter sides in any given order	
 			//Find the type 
 			triangleType(s_one, s_two, s_three, a_plus_b, a_plus_c, b_plus_c, longest_side);
 		}
 		else if(s_two > s_one && s_two > s_three){
 			longest_side = s_two;
+			s_two = s_three; //this allows user to enter sides in any given order		
 			//Find the type 
 			triangleType(s_one, s_two, s_three, a_plus_b, a_plus_c, b_plus_c, longest_side);
 		}
 		if(s_three > s_one && s_three > s_two){
 			longest_side = s_three;
+			s_three = s_one; //this allows user to enter sides in any given order
 			//Find the type 
 			triangleType(s_one, s_two, s_three, a_plus_b, a_plus_c, b_plus_c, longest_side);
 		}
 	}
 	else{
 		printf("\n\nNOT A VALID TRIANGLE\n\n");
-		return 1;
+		return 1; //end the program if input is not valid 
 	}
-
 	return 0;
 }
 
@@ -83,7 +83,7 @@ int triangleType(s_one, s_two, s_three, a_plus_b, a_plus_c, b_plus_c, longest_si
 	//Figure out type of triangle
 	int right_tri_check_one, right_tri_check_two, right_tri_check_longest;
 
-	right_tri_check_one = pow(s_one, 2);
+	right_tri_check_one = pow(s_one, 2); //making it s_three instead of s_one seems to get rid of "input-order" error
 	right_tri_check_two = pow(s_two, 2);
 	right_tri_check_longest = pow(longest_side, 2);
 
@@ -95,26 +95,24 @@ int triangleType(s_one, s_two, s_three, a_plus_b, a_plus_c, b_plus_c, longest_si
 	{
 		printf("\nRIGHT TRIANGLE\n");
 	}
+	//PLUG AND PLAY, LOGICAL ERROR 9/18/18
 	else if (right_tri_check_one + right_tri_check_two < right_tri_check_longest)
 	{
 		printf("\nOBTUSE TRIANGLE\n");
 	}
+	//PLUG AND PLAY, LOGICAL ERROR 9/18/18
 	else if (right_tri_check_one + right_tri_check_two > right_tri_check_longest)
 	{
 		printf("\nACUTE TRIANGLE\n");
 	}
 	else {
-		printf("\nNOT EQUILATERAL TRIANGLE");		
-		printf("\nNOT RIGHT TRIANGLE\n");		
-		printf("\nNOT OBTUSE TRIANGLE\n");	
-		printf("\nNOT ACUTE TRIANGLE\n");	
-		printf("\nNOT ISOSCELES TRIANGLE\n");	
+		return 1;
 	}
 
-	printf("Longest Side: %d\n", longest_side);
+	printf("\n\nLongest Side: %d\n", longest_side);
 	printf("%d\n", right_tri_check_one);
 	printf("%d\n", right_tri_check_two);
 	printf("%d\n", right_tri_check_longest);
 
 	return 0;
-}
+} 
