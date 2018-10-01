520c5934ee1eb7a20a33aaa19a3a7afb2d15b044
John Glatts <33476958+jglatts@users.noreply.github.com>
2018-09-30 18:23:11 -0700

    Update cis_hw_two.c

--- cis_hw_two.c
+++ cis_hw_two.c
@@ -2,7 +2,7 @@
 
 CIS 1057 HW 2
 Author: John Glatts
-Program: Triangle Determanation and Area Calculation
+Program: Triangle Determination and Area Calculation
 Contact: tug13361@gmail.com
 Due Date: 9/27/18 
 
@@ -24,6 +24,7 @@ int main(int argc, char const *argv[]) {
     double side_one, side_two, side_three;  //user input for triangle sides 
 
     printf("\nEnter Length of Side 1\n");
+    printf("l");
     scanf("%lf", &side_one);
     printf("\nEnter Length of Side 2\n");
     scanf("%lf", &side_two);
@@ -66,20 +67,20 @@ double triangleValid(double s_one, double s_two, double s_three){
         {     
             //find the longest side 
             if(s_one > s_two && s_one > s_three){
-                printf("\nISOCELES TRIANGLE");
-                printf("\tALSO: ");
+                printf("ISOSCELES TRIANGLE");
+                printf("ALSO: ");
                 longest_side = s_one;   
                 triangleType(s_two, s_three, longest_side); //find the type
             }
             else if(s_two > s_one && s_two > s_three){
-                printf("\nISOCELES TRIANGLE\n");
-                printf("\tALSO: ");                
+                printf("ISOSCELES TRIANGLE\n");
+                printf("ALSO: ");                
                 longest_side = s_two;
                 triangleType(s_one, s_three, longest_side); //find the type
             }
             else{
-                printf("\nISOCELES TRIANGLE\n");
-                printf("\tALSO: ");                
+                printf("ISOSCELES TRIANGLE\n");
+                printf("ALSO: ");                
                 longest_side = s_three; 
                 triangleType(s_one, s_two, longest_side);   //find the type
             }
@@ -112,7 +113,7 @@ double triangleType(double side_a, double side_b, double side_c){
     //Find Area using Heron's Formula
     double side_a_squared, side_b_squared, side_c_squared;
     double formula_s, multiply_sides, triangle_area;
-
+/
     side_a_squared = pow(side_a, 2); 
     side_b_squared = pow(side_b, 2);
     side_c_squared = pow(side_c, 2);
@@ -124,23 +125,21 @@ double triangleType(double side_a, double side_b, double side_c){
 
     if (side_a_squared + side_b_squared == side_c_squared)
     {
-        printf("RIGHT TRIANGLE\n");
+        printf("RIGHT TRIANGLE");
     }
     else if (side_a_squared + side_b_squared < side_c_squared)
     {
-        printf("OBTUSE TRIANGLE\n");
+        printf("OBTUSE TRIANGLE");
     }
     else if (side_a_squared + side_b_squared > side_c_squared)
     {
-        printf("ACUTE TRIANGLE\n");
+        printf("ACUTE TRIANGLE");
     }
     else {
         return 1;
     }
 
-    printf("\n\nLongest Side: %lf\n", side_c);
-    printf("S in Heron's Formula = %lf\n", formula_s);
-    printf("Area = %lf\n", triangle_area);
+    printf("\nArea = %lf\n", triangle_area);
 
 
     return 0;
